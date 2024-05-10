75f42f88b2df1494aa043343f49c1ed28eb5f250
Salim <dalgali.salim@ogr.ahievran.edu.tr>
2024-05-10 19:51:37 +0300

    Dosya işlemleri sayfasındaki kalan tüm slotlar oluşturuldu ve sinyallere bağlandı

--- LinuxSystemManager.py
+++ LinuxSystemManager.py
@@ -1,6 +1,8 @@
+import os
 import sys
 import paramiko
-from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QInputDialog, QMessageBox, QLineEdit, QTableWidgetItem
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QInputDialog, QMessageBox, QLineEdit, QTableWidgetItem, \
+    QFileDialog
 from PyQt6.QtCore import QUrl
 from PyQt6.QtGui import QDesktopServices
 from MainGUI import Ui_MainWindow
@@ -25,7 +27,10 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.toolButton_delete_file.clicked.connect(self.delete_file)
         self.toolButton_copy_file.clicked.connect(self.copy_file)
         self.toolButton_move_file.clicked.connect(self.move_file)
-
+        self.toolButton_file_perm.clicked.connect(self.change_perm)
+        self.toolButton_edit_file.clicked.connect(self.edit_file)
+        self.toolButton_upload_file.clicked.connect(self.upload_file)
+        self.toolButton_download_file.clicked.connect(self.download_file)
 
 
     def change_user(self):
@@ -123,15 +128,81 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 file_to_copy = self.tableWidget_file.item(row, column).text()
                 copy_dir, _ = QInputDialog.getText(self, "Dosya Taşıma", "Dosyanın taşınacağı dizini girin.")
                 command = "mv" + " " + file_to_copy.strip() + " " + copy_dir
-                print(command)
                 stdin, stdout, stderr = self.ssh_client.exec_command(command)
                 QMessageBox.information(self, "Dosya Taşıma", "Dosya başarıyla taşındı.")
                 self.search_file()
             except Exception as e:
                 QMessageBox.warning(self, "Dosya Taşıma", "Dosya taşınırken bir hata oluştu.")
-                print(e)
         else:
             QMessageBox.warning(self, "Dosya Taşıma", "Lütfen bir dosya seçin.")
+
+    def change_perm(self):
+        selected_items = self.tableWidget_file.selectedItems()
+        if selected_items:
+            try:
+                row = selected_items[0].row()
+                column = selected_items[0].column()
+                file_to_perm = self.tableWidget_file.item(row, column).text()
+                mod, _ = QInputDialog.getText(self, "Dosya İzinleri", "Değiştirmek istediğiniz iznin mod değerini girin.")
+                command = "chmod" + " " + mod + " " + file_to_perm
+                stdin, stdout, stderr = self.ssh_client.exec_command(command)
+                QMessageBox.information(self, "Dosya İzinleri", "Dosya izni başarıyla değiştirildi.")
+            except Exception as e:
+                QMessageBox.warning(self, "Dosya İzinleri", "Dosya izni değiştirilirken bir hata oluştu.")
+                print(e)
+        else:
+            QMessageBox.warning(self, "Dosya İzinleri", "Lütfen bir dosya seçin.")
+
+    def edit_file(self):
+        selected_items = self.tableWidget_file.selectedItems()
+        if selected_items:
+            try:
+                row = selected_items[0].row()
+                column = selected_items[0].column()
+                file_to_edit = self.tableWidget_file.item(row, column).text()
+                command = f"cat {file_to_edit}"
+                stdin, stdout, stderr = self.ssh_client.exec_command(command)
+                file_content = stdout.read().decode()
+                editor_window.textEdit.setPlainText(file_content)
+                editor_window.ssh_client = self.ssh_client
+                editor_window.file_to_edit = file_to_edit
+                editor_window.show()
+            except Exception as e:
+                QMessageBox.warning(self, "Dosya Edit", "Dosya editlenirken bir hata oluştu.")
+                print(e)
+        else:
+            QMessageBox.warning(self, "Dosya Edit", "Lütfen bir dosya seçin.")
+
+    def upload_file(self):
+        try:
+            file_path, _ = QFileDialog.getOpenFileName(self, "Dosya Seç", ".")
+            path_to_upload, _ = QInputDialog.getText(self, "Dosya Yükleme", "Dosyanın yükleneceği dizini girin.")
+            file_name = file_path.split("/")[-1]
+            full_path = path_to_upload + "/" +  file_name
+            sftp_client = self.ssh_client.open_sftp()
+            sftp_client.put(file_path, full_path)
+            sftp_client.close()
+            QMessageBox.information(self, "Dosya Yükleme", "Dosya başarıyla yüklendi.")
+        except Exception as e:
+            QMessageBox.warning(self, "Dosya Yükleme", "Dosya yüklenirken bir hata oluştu.")
+
+    def download_file(self):
+        selected_items = self.tableWidget_file.selectedItems()
+        if selected_items:
+            try:
+                row = selected_items[0].row()
+                column = selected_items[0].column()
+                file_to_download = self.tableWidget_file.item(row, column).text()
+                path_to_download, _ = QInputDialog.getText(self, "Dosya İndirme", "Dosyanın indirileceği dizini girin.")
+                full_path = path_to_download + "/" + file_to_download.strip().split("/")[-1]
+                sftp_client = self.ssh_client.open_sftp()
+                sftp_client.get(file_to_download.strip(), full_path)
+                sftp_client.close()
+            except Exception as e:
+                QMessageBox.warning(self, "Dosya İndirme", "Dosya indirilirken bir hata oluştu.")
+                print(e)
+        else:
+            QMessageBox.warning(self, "Dosya İndirme", "Lütfen bir dosya seçin.")
 
 
 class Login_Window(QWidget, Login_Form):
@@ -166,6 +237,17 @@ class Editor_Window(QWidget, Editor_Form):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+
+        self.ssh_client = None
+        self.file_to_edit = None
+
+        self.pushButton_save_file.clicked.connect(self.save_file)
+
+    def save_file(self):
+        content = self.textEdit.toPlainText()
+        stdin, stdout, stderr = self.ssh_client.exec_command(f"echo '{content}' > {self.file_to_edit}")
+        QMessageBox.information(self, "Dosya Edit", "Dosya içeriği başarıyla değiştirildi.")
+        self.close()
 
 app = QApplication(sys.argv)
 editor_window = Editor_Window()
